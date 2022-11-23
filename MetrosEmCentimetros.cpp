87d5649ff68c3a7224ae310e7fcc2ec04b398a95
Victor Ogatha <116044128+Vtogatha@users.noreply.github.com>
2022-11-22 21:35:14 -0300

    Add files via upload

---
+++ MetrosEmCentimetros.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	int metros, centimetros;
+	
+	printf("Digite o valor em metros:\n");
+	scanf("%d", &metros);
+	
+	centimetros = (metros * 100);
+	
+	printf("%d metros em centimetros sao %d", metros, centimetros);
+	
+	
+	
+	
+	
+	
+}
