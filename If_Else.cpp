6ce3eb28d05eba9d0d1b0bfff26d23173359fcf5
Victor Ogatha <116044128+Vtogatha@users.noreply.github.com>
2022-11-22 21:34:42 -0300

    Add files via upload

---
+++ If_Else.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n;
+	
+	printf("Digite um valor:\n");
+	scanf("%d", &n);
+	
+	if(n > 0){
+		printf("O numero %d e positivo", n);
+	}else{
+
+		printf("Valor negativo %d", n);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
