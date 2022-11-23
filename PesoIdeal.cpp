55e9e156878bf93fa88f8ee7f3dd8c4f2c906361
Victor Ogatha <116044128+Vtogatha@users.noreply.github.com>
2022-11-22 21:30:19 -0300

    Add files via upload

---
+++ PesoIdeal.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	
+	float altura, peso_ideal;
+	
+	printf("Digite a altura:\n");
+	scanf("%f", &altura);
+	
+	peso_ideal = (72.7 * altura)-58;
+	
+	printf("O peso ideal e:%.2f", peso_ideal);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
